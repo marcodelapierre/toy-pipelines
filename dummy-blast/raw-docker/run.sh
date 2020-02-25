39b7673e41383ad07dc44e83411a10375b3935ff
Marco De La Pierre <marco.delapierre@gmail.com>
2020-02-25 05:32:27 +0000

    Revert "run.sh fix for blast/raw"
    This reverts commit 5d3f7f93472e0b326d7d645f400b8eae52a2218b.

--- dummy-blast/raw-docker/run.sh
+++ dummy-blast/raw-docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./blastp -db pdb/tiny -query query.fa -outfmt 6 2>/dev/null | head -n 10 | cut -f 2 > top_hits
+./blastp -db pdb/tiny -query query.fa -outfmt 6 | head -n 10 | cut -f 2 > top_hits
 
-./blastdbcmd -db pdb/tiny -entry_batch top_hits 2>/dev/null | head -n 10 > sequences
+./blastdbcmd -db pdb/tiny -entry_batch top_hits | head -n 10 > sequences
